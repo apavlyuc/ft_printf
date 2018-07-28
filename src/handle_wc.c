b6d92c1a791ebbb1ab38d932cc500175036deff9
Alexander PAVLYUCHUK <apavlyuc@e1r2p10.unit.ua>
2018-07-28 18:42:09 +0300

    refactor according to norme

--- src/handle_wc.c
+++ src/handle_wc.c
@@ -6,18 +6,18 @@
 /*   By: apavlyuc <apavlyuc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/28 17:30:15 by apavlyuc          #+#    #+#             */
-/*   Updated: 2018/07/28 17:52:26 by apavlyuc         ###   ########.fr       */
+/*   Updated: 2018/07/28 18:40:30 by apavlyuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/ft_printf.h"
 #include <stdlib.h>
 
-int     handle_lc(char **dst, va_list *args, t_param *param)
+int		handle_lc(char **dst, va_list *args, t_param *param)
 {
-    *dst = (char *)malloc(1);
-    (void)dst;
-    (void)args;
-    (void)param;
-    return (0);
+	*dst = (char *)malloc(1);
+	(void)dst;
+	(void)args;
+	(void)param;
+	return (0);
 }
